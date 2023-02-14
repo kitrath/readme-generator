1b70c921fc8493f39ccc3193a61d7aae3ed1251d
kitrath <crathmel@gmail.com>
2023-02-14 13:08:02 -0600

    add generated readme document structure

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -8,11 +8,50 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { return ''; }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+
+# ${data.title}
+
+${renderLicenseBadge(data.license)}
+## Description
+${data.description}
+
+## Table of Contents
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [License](#license)
+4. [Contributing](#contributing)
+5. [Tests](#tests)
+6. [Questions](#questions);
+
+## [Installation](#installation)
+<a name="installation></a>
+${data.installation}
+
+## [Usage](#usage)
+<a name="usage"></a>
+${data.usage}
+
+## [License](#license)
+<a name="license"></a>
+${renderLicenseSection(data.license)}
+
+## [Contributing](#contributing)
+<a name="contributing"></a>
+${data.contributing}
+
+## [Tests](#tests)
+<a name="tests"></a>
+${data.tests}
+
+## [Questions](#questions)
+<a name="questions"></a>
+${data.questions}
+
 
 `;
 }
